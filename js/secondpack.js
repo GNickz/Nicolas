d069d77321549d0f1f7a95d0d1a3cea74ed1de3f
Nicolas Urrutia <urrutiagonzalez.gustavon@gail.com>
2024-03-29 21:58:04 -0600

    Solucion errores de estilos

--- js/secondpack.js
+++ js/secondpack.js
@@ -50,10 +50,10 @@ trigger.addEventListener('click',()=>{
 /*Slider*/
 
 let imagenes = [
-    "/assets/Imagen-Uno.webp",
-    "/assets/Imagen-Dos.webp",
-    "/assets/Imagen-Tres.webp",
-    "/assets/Imagen-Cuatro.webp"
+    "assets/Imagen-Uno.webp",
+    "assets/Imagen-Dos.webp",
+    "assets/Imagen-Tres.webp",
+    "assets/Imagen-Cuatro.webp"
 ];
 document.Imagen.src = imagenes[0];
 
